0be4b9887965c104437a097aadb85dff9ee33a0b
Pedram Rastegar <peddy.rast@gmail.com>
2017-12-31 11:52:48 +0330

    Copied Styles-folder images-folder fontawesome-folder from Old project

---
+++ src/images/edit.svg.js
@@ -0,0 +1,5 @@
+import React from "react";
+export default <svg viewBox="0 0 94 94" className="svg-icon">
+    <path
+        d="M29 50l37 -36 15 15 -37 37 -15 -1 0 -15zm-14 -50l45 0 0 13 -47 0 0 68 68 0 0 -47 13 0 0 45c0,8 -7,15 -15,15l-64 0c-8,0 -15,-7 -15,-15l0 -64c0,-8 7,-15 15,-15zm56 9l8 -9 15 15 -8 9 -15 -15z"/>
+</svg>;
---
+++ src/images/icons.js
@@ -0,0 +1,22 @@
+import certificateIcon from "./certificate.svg";
+import editIcon from "./edit.svg";
+import educationIcon from "./education.svg";
+import researchIcon from "./research.svg";
+import skillIcon from "./skill.svg";
+import starIcon from "./star.svg";
+import streamIcon from "./stream.svg";
+import userInfoIcon from "./userinfo.svg.js";
+import workExperienceIcon from "./workExperience.svg";
+
+
+export {
+    editIcon,
+    certificateIcon,
+    educationIcon,
+    streamIcon,
+    researchIcon,
+    skillIcon,
+    starIcon,
+    userInfoIcon,
+    workExperienceIcon,
+}
