9de2786c1669282e88bcadf133f93cabd287cae1
mohsen <mohsen.workman@gmail.com>
2018-02-18 11:28:14 +0330

    in middle of Process : adding list of posts to Home page

---
+++ src/views/common/inputs/AttachFile.js
@@ -0,0 +1,105 @@
+/*global __*/
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {createFile} from "../../../crud/media/media";
+
+class AttachFile extends Component {
+  static defaultProps = {
+    customValidate: () => false,
+    required: false,
+  };
+
+  static propTypes = {
+    required: PropTypes.bool,
+    customValidate: PropTypes.func,
+    // TODO mohsen: fileType: PropTypes.arrayOf(PropTypes.string.isRequired),
+    // TODO mohsen: fileSize
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {error: false, isLoading: false, fileName:'', media: {}};
+  };
+
+  _createFile = (fileString, fileName) => {
+    const mediaResult = (res) => {
+      this.setState({...this.state, isLoading: false, fileName, media:res});
+    };
+    createFile(fileString, mediaResult);
+  };
+
+  _handleChange = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const error = this._validateFile(file);
+    this.setState({...this.state, error});
+    if (file && !error) {
+      // TODO mohsen: check maximum file-size with attention to fileType
+      let reader = new FileReader();
+      reader.onloadstart = () => {
+        this.setState({isLoading: true});
+      };
+      reader.onloadend = () => {
+        const fileName = file.name;
+        this._createFile(reader.result, fileName);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  _onChangeClick = () => {
+    this.setState({...this.state, isLoading: false, fileName:'', media:{}});
+  };
+
+  _validateFile = (file) => {
+    const {required, customValidate} = this.props;
+    if (required) {
+      if (!file) {
+        return __('Required field');
+      }
+    }
+    return customValidate(file);
+  };
+
+  _validate = () => {
+    const error = this._validateFile(this.file);
+    this.setState({error});
+    return error;
+  };
+
+  _getFile = () => {
+    return this.state.media;
+  };
+
+  _getFileName = () => {
+    return this.state.fileName;
+  };
+
+  render() {
+    const {error, isLoading} = this.state;
+    if (isLoading) {
+      return (
+          <span>{__('Uploading...')}</span>
+      )
+    }
+    return (
+      <span>
+        <label for="file">
+          <i class="fa fa-paperclip" aria-hidden="true"></i>
+        </label>
+        <input
+          type="file"
+          className="custom-file-input w-100"
+          onChange={this._handleChange}
+          onClick={this._onChangeClick}
+          id="file"
+          hidden
+        />
+        {error &&
+        <div className="form-control-feedback">{error}</div>}
+      </span>
+    )
+  }
+}
+
+export default AttachFile;
--- src/views/pages/home/CreatPostHome.js
+++ src/views/pages/home/CreatPostHome.js
@@ -1,53 +1,148 @@
-import React, {Component} from "react"
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 import "moment/locale/fa";
 import {defaultImg} from "src/images/icons";
-
+import AttachFile from "src/views/common/inputs/AttachFile";
+import {updateUser} from "../../../crud/user/user";
 
 class CreatePostFooter extends Component {
 
-    render() {
-        return (
-            <div className="-createPostFooter">
-                <div>
-                    <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
-                    <i class="fa fa-shopping-cart mr-3" aria-hidden="true"></i>
-                </div>
-                <div>
-                     <i class="fa fa-paperclip" aria-hidden="true"></i>
-                     <i class="fa fa-smile-o mr-3" aria-hidden="true"></i>
-                     <span className="mr-4">
-                         <span style={{color:"#BFBFBF"}}>ارسال</span>
-                         <i class="fa fa-paper-plane mr-2" style={{color:"#929292"}} aria-hidden="true"></i>
-                     </span>
-                </div>
-            </div>
-        )
+  static propTypes = {
+    getAttachMedia: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {postType:'post'}
+  }
+
+  _AttachFile = () => {
+    return {
+      media: this.AttachFileInput._getFile(),
+      fileName: this.AttachFileInput._getFileName(),
+      validate: this.AttachFileInput._validate(),
     }
+  };
+
+  _post_type = () => {
+    return this.state.postType
+  };
+
+  _handle_post_type = (e) => {
+    e.preventDefault();
+
+  };
+
+  render() {
+    return (
+      <div className="-createPostFooter">
+        <div>
+          <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
+          <i class="fa fa-shopping-cart mr-3" aria-hidden="true"></i>
+        </div>
+        <div>
+          <AttachFile
+            ref={AttachFileInput => {
+              this.AttachFileInput = AttachFileInput
+            }}
+          />
+          <i class="fa fa-smile-o mr-3" aria-hidden="true"></i>
+          <span className="mr-4">
+             <span style={{color: "#BFBFBF"}}>ارسال</span>
+             <label for="submit" className="-submitAttach">
+               <i class="fa fa-paper-plane mr-2" aria-hidden="true"></i>
+             </label>
+            <input id="submit" hidden type="submit"/>
+          </span>
+        </div>
+      </div>
+    )
+  }
 }
 
 class HomeCreatePost extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {media: {}, fileName:''}
+  }
 
-    render() {
-        const {profile_media_File} = this.props;
-        return (
-            <div className="-createPostHome">
-                <div className="-img-col">
-                    {/*// TODO mohsen: handle src of img*/}
-                    <img className="-item-imgPost rounded-circle" src={profile_media_File || defaultImg} alt=""/>
-                </div>
-                <div className="-content-col">
-                    <textarea
-                        className="border-0 h-75 -outlineWhite"
-                    />
-                    <CreatePostFooter />
-                </div>
-            </div>
-        )
+  _getValues = () => {
+    const {media} = this.state;
+    const mediaId = media ? media.id : null;
+    return {
+      post_type: this.postTypeInput.getValue(),
+      post_title: this.postTitleInput.getValue(),
+      post_description: this.postDescriptionInput.getValue(),
+      post_pinned: this.postPinnedInput.getValue(),
+      post_picture: mediaId,
+      // post_parent: this.props
     }
+  };
+
+  _formValidate = () => {
+    let result = true;
+    const validates = [
+      this.createPostFooter.validate(),
+      this.firstNameInput.validate(),
+      this.lastNameInput.validate(),
+      this.emailInput.validate()
+    ];
+    for (let i = 0; i < validates.length; i++) {
+      if (validates[i]) {
+        result = false;
+        break;
+      }
+    }
+    return result
+  };
+
+  _getAttachMedia = (media, fileName) => {
+    this.setState({...this.state, media, fileName})
+  };
+
+
+  _save = () => {
+    const userId = this.props.user.id;
+    const formValues = this._getValues();
+    return updateUser(formValues, userId)
+  };
+
+  _onSubmit = (e) => {
+    e.preventDefault();
+    if (this._formValidate()) {
+      this._save()
+    }
+    return false;
+  };
+
+  render() {
+    const {media, fileName} = this.state;
+    const {profile_media_file} = this.props;
+    return (
+      <form className="-createPostHome" onSubmit={this._onSubmit} >
+        {/*// TODO mohsen: handle src of img*/}
+        <img className="-img-col rounded-circle" src={profile_media_file || defaultImg} alt="" />
+        <div className="-content-col">
+          <div className="d-flex flex-row mb-2">
+            <textarea
+              className="-createPostInput"
+              name="post_description"
+            />
+            <div className="-img-content text-center">
+              {(media.file) ? <img src={media.file} alt="imagePreview"/> : ('')}
+              <span style={{color: "#BFBFBF"}}>{fileName}</span>
+            </div>
+          </div>
+          <CreatePostFooter
+            getAttachMedia={this._getAttachMedia}
+            ref={createPostFooter => {
+              this.createPostFooter = createPostFooter
+            }}
+          />
+        </div>
+      </form>
+    )
+  }
 }
 
 export default HomeCreatePost;
