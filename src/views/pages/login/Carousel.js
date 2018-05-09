a0343ba974cc4ee292ec4ec474e2352695adaa5f
aliorooji <ali.orooji.ilia@gmail.com>
2018-05-09 19:04:09 +0430

    initial state of login slide without autoSlide

--- src/views/pages/login/Carousel.js
+++ src/views/pages/login/Carousel.js
@@ -1,48 +1,97 @@
-import React, {Component} from 'react';
-
-const Carousel1 = () => {
-	return (
-			<div>
-				<h2>بازار یابی</h2>
-				<p>اکوسیستم دانش بنیان با بستر بورس و دانش نخبگان</p>
-			</div>
-	)
-};
-const Carousel2 = () => {
-	return (
-			<div>
-				<h2>بازارگردانی</h2>
-				<p>شیکه ارتباطی تسهیلگر</p>
-			</div>
-	)
-};
-const Carousel3 = () => {
-	return (
-			<div>
-				<h2>بازار سازی</h2>
-				<p>با استفاده از امکانات بین المللی</p>
-			</div>
-	)
-};
+import React, { Component } from 'react'
+import uuid from 'uuid'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 class CarouselLogin extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-    	slides:[1,2,3],
-    	active: 1,
+    constructor() {
+        super()
+        this.state = {
+			items: [
+				{
+					id: `login-slide-${uuid()}`,
+					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۱',
+					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+				 },
+				 {
+					id: `login-slide-${uuid()}`,
+					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۲',
+					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+				 },
+				 {
+					id: `login-slide-${uuid()}`,
+					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۳',
+					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+				 },
+			],
+			activeId: 1,
+			showSlide: false,
+			showDesc: false,
 		}
-  }
-  
-  render () {
-		return (
-      <div className=" carousel-wrapper">
-				<Carousel1/>
-				<Carousel2/>
-				<Carousel3/>
-      </div>
-		)
-  }
+	}
+	componentDidMount() { this.setState({ ...this.state, activeId: this.state.items[0].id, showSlide: true }) }
+
+	_contentHandler = (id) => this.setState({ ...this.state, activeId: id, showSlide: false }, () => console.log('f', this.state.show))
+
+	render() {
+        const { items, activeId, showSlide, showDesc } = this.state
+        return (
+            <div className="carousel-wrapper">
+				<BtnBar activeId={activeId} items={items} handler={this._contentHandler} />
+					<div>
+						<TransitionGroup>
+							{items.map(item => (item.id === activeId ? 
+								<CSSTransition
+									in={item.id === activeId}
+									timeout={1000}
+									classNames="login-slide"
+									unmountOnExit
+									onExited={() => this.setState({ ...this.state, showSlide: true, showDesc: false }, () => console.log('s', this.state.show))}
+								>
+									<div className="login-slide" key={`content${item.id}`}>
+										<CSSTransition
+											in={showSlide}
+											timeout={1000}
+											classNames="login-slide"
+											unmountOnExit
+											onEntered={() => this.setState({showDesc: true})}
+										>
+											<div className="content">
+												<header>{item.header}</header>
+												<br />
+												<p>{item.desc}</p>
+											</div>
+											
+										</CSSTransition>
+									</div>
+								</CSSTransition>
+								:
+								''
+								) 
+							)}
+						</TransitionGroup>
+					</div>
+            </div>
+        )
+    }
 }
+
+const BtnBar = ({ items, activeId, handler }) => (
+	<div className="carousel-btn-bar">
+		<ul>
+			{items.map(item => 
+				<li key={`btn${item.id}`}>
+					<i
+						onClick={handler.bind(null, item.id)}
+						className={
+							activeId === item.id ? 'fa fa-circle'
+							: 
+							'fa fa-circle-o'
+						} 
+					/>
+				</li>
+			)}
+		</ul>
+	</div>
+)
 
 export default CarouselLogin;
