2eead4805b168a3943940bea426f77a2fd431218
mohsen <mohsen.workman@gmail.com>
2018-05-16 18:31:08 +0430

    scaling loginPage part1

--- src/views/pages/login/HeaderLogin.js
+++ src/views/pages/login/HeaderLogin.js
@@ -7,13 +7,13 @@ const HeaderLogin = ({iosLink, androidLink, address, phoneNumber, logoCaption}) 
 	return (
 			<div className="header">
 				<div className="logo-wrapper">
-					<div className="logo"><img src={logoDaneshBoom} alt="" /></div>
+					<div className="logo"><img className="h-48px" src={logoDaneshBoom} alt="" /></div>
 					<div className="company-name">{__('Danesh Boom')}</div>
 					<div className="divider"></div>
 					<div className="logo-caption">{logoCaption}</div>
 				</div>
 				<div className="address-wrapper">
-					<div className="address-section">
+					<div className="address-section ml-5">
 						<div className='icon phone-icon'>
 							<FontAwesome name="phone"/>
 						</div>
