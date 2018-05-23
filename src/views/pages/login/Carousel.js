3489c00b1e07613da86291e31fb09ac48001f5f2
mohsen <mohsen.workman@gmail.com>
2018-05-23 11:43:32 +0430

    review fonts and responsive login page

--- src/views/pages/login/Carousel.js
+++ src/views/pages/login/Carousel.js
@@ -1,126 +1,128 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import uuid from 'uuid'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 // main component
 class CarouselLogin extends Component {
-    constructor() {
-        super()
-        this.state = {
-			slides: [
-				{
-					id: `login-slide-${uuid()}`,
-					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۱',
-					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
-				 },
-				 {
-					id: `login-slide-${uuid()}`,
-					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۲',
-					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
-				 },
-				 {
-					id: `login-slide-${uuid()}`,
-					header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۳',
-					desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
-				 },
-			],
-			activeId: 1,
-			showSlide: false,
-		}
-	}
-	componentDidMount() {
-		this._contentHandler(this.state.slides[0].id) //  the first content setting
-		setTimeout(() => this.setState({ ...this.state, showSlide: true }), 100) // set showSlide: true after 10ms
-	 }
-	 
-	componentWillUnmount() {
-		clearInterval(this.interval);
-	}
-	
-	_contentHandler = (id) => {
-		clearInterval(this.interval) // first clearing past interval
-		const { slides } = this.state
-		this.setState({ ...this.state, activeId: id, showSlide: false })
-		let index = slides.findIndex(slide => slide.id === id)
-		if (index === slides.length -1) index = 0
-		else index++
-		this.interval = setInterval(this._contentHandler, 5000, slides[index].id) // set new interval
-	}
 
-	render() {
-        const { slides, activeId, showSlide } = this.state
-        return (
-            <div className="carousel-wrapper">
-				<BtnBar activeId={activeId} slides={slides} handler={this._contentHandler} />
-					<div>
-						<TransitionGroup>
-							{slides.map(slide => (slide.id === activeId ?
-								// <---------------- slide
-								<CSSTransition // start of next slide entering is onExited the prev slide.
-									key={`content${slide.id}`}
-									in={slide.id === activeId}
-									timeout={1000}
-									classNames="login-slide"
-									unmountOnExit
-									onExited={() => this.setState({ ...this.state, showSlide: true})}
-								>
-									<div>
-										<CSSTransition
-											in={showSlide}
-											timeout={1000}
-											classNames="login-slide"
-											unmountOnExit
-										>
-											{
-												status => (
-													<div className="login-slide">
-														<CSSTransition // when the slide is entered header of slide start entering 
-															in={status === 'entering' || status === 'entered'}
-															timeout={1000}
-															classNames="login-slide-header"
-															unmountOnExit
-														>	
-															<header className="login-slide-header">{slide.header}</header>		
-														</CSSTransition>
-														<br />
-														<p className="login-slide-desc">{slide.desc}</p>
-													</div>
-												)
-											}
-										</CSSTransition>
-									</div>
-								</CSSTransition>
-								:
-								''
-								) 
-								// slide ------------------->
-							)}
-
-						</TransitionGroup>
-					</div>
-            </div>
-        )
+  constructor() {
+    super()
+    this.state = {
+      slides: [
+        {
+          id: `login-slide-${uuid()}`,
+          header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۱',
+          desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+        },
+        {
+          id: `login-slide-${uuid()}`,
+          header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۲',
+          desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+        },
+        {
+          id: `login-slide-${uuid()}`,
+          header: 'بازاریابی, بازارسازی و بازگردانی تخصصی در حوزه ملی و بین‌المللی۳',
+          desc: 'این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.این توضیحات مربوط به یک آیتم خاص است.',
+        },
+      ],
+      activeId: 1,
+      showSlide: false,
     }
+  }
+
+  componentDidMount() {
+    this._contentHandler(this.state.slides[0].id) //  the first content setting
+    setTimeout(() => this.setState({...this.state, showSlide: true}), 100) // set showSlide: true after 10ms
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  _contentHandler = (id) => {
+    clearInterval(this.interval) // first clearing past interval
+    const {slides} = this.state
+    this.setState({...this.state, activeId: id, showSlide: false})
+    let index = slides.findIndex(slide => slide.id === id)
+    if (index === slides.length - 1) index = 0
+    else index++
+    this.interval = setInterval(this._contentHandler, 5000, slides[index].id) // set new interval
+  }
+
+  render() {
+    const {slides, activeId, showSlide} = this.state
+    return (
+      <div className="carousel-wrapper">
+        <BtnBar activeId={activeId} slides={slides} handler={this._contentHandler}/>
+        <div>
+          <TransitionGroup>
+            {slides.map(slide => (slide.id === activeId ?
+                // <---------------- slide
+                <CSSTransition // start of next slide entering is onExited the prev slide.
+                  key={`content${slide.id}`}
+                  in={slide.id === activeId}
+                  timeout={1000}
+                  classNames="login-slide"
+                  unmountOnExit
+                  onExited={() => this.setState({...this.state, showSlide: true})}
+                >
+                  <div>
+                    <CSSTransition
+                      in={showSlide}
+                      timeout={1000}
+                      classNames="login-slide"
+                      unmountOnExit
+                    >
+                      {
+                        status => (
+                          <div className="login-slide">
+                            <CSSTransition // when the slide is entered header of slide start entering
+                              in={status === 'entering' || status === 'entered'}
+                              timeout={1000}
+                              classNames="login-slide-header"
+                              unmountOnExit
+                            >
+                              <header className="login-slide-header">{slide.header}</header>
+                            </CSSTransition>
+                            <br/>
+                            <p className="login-slide-desc">{slide.desc}</p>
+                          </div>
+                        )
+                      }
+                    </CSSTransition>
+                  </div>
+                </CSSTransition>
+                :
+                ''
+              )
+              // slide ------------------->
+            )}
+
+          </TransitionGroup>
+        </div>
+      </div>
+    )
+  }
 }
 
 // slide show controle btn bar
-const BtnBar = ({ slides, activeId, handler }) => (
-	<div className="carousel-btn-bar">
-		<ul>
-			{slides.map(slide => 
-				<li key={`btn${slide.id}`}>
-					<i
-						onClick={handler.bind(null, slide.id)}
-						className={
-							activeId === slide.id ? 'fa fa-circle'
-							: 
-							'fa fa-circle-o'
-						}
-					/>
-				</li>
-			)}
-		</ul>
-	</div>
+const BtnBar = ({slides, activeId, handler}) => (
+  <div className="carousel-btn-bar">
+    <ul>
+      {slides.map(slide =>
+        <li key={`btn${slide.id}`}>
+          <i
+            onClick={handler.bind(null, slide.id)}
+            className={
+              activeId === slide.id ? 'fa fa-circle'
+                :
+                'fa fa-circle-o'
+            }
+          />
+        </li>
+      )}
+    </ul>
+  </div>
 )
 
 export default CarouselLogin;
--- src/views/pages/login/signInForm.js
+++ src/views/pages/login/signInForm.js
@@ -9,102 +9,102 @@ import ErrorMessage from './ErrorMessage'
 import cookies from 'browser-cookies'
 
 export default class LoginForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			message: '',
-			error: null,
-			rememberme: null,
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      error: null,
+      rememberme: null,
+    }
+  }
 
-	componentDidMount() {
-		const {handleLogIn} = this.props;
-		const {rememberme} = this.state;
-		socket.on("TOKEN_Result", res => {
-			if (res.non_field_errors) {
-				const message = res.non_field_errors[0];
-				this._handleError(message);
-				return false;
-			}
-			if (res.password || res.username) {
-				const message = "Fields should not be empty";
-				this._handleClick(message)
-			}
-			handleLogIn(res , rememberme);
-		});
-	}
+  componentDidMount() {
+    const {handleLogIn} = this.props;
+    const {rememberme} = this.state;
+    socket.on("TOKEN_Result", res => {
+      if (res.non_field_errors) {
+        const message = res.non_field_errors[0];
+        this._handleError(message);
+        return false;
+      }
+      if (res.password || res.username) {
+        const message = "Fields should not be empty";
+        this._handleClick(message)
+      }
+      handleLogIn(res, rememberme);
+    });
+  }
 
-	_handleError = (msg) => {
-		const message = 'نام کاربری یا گذرواژه صحیح نمی باشد';
-		this.setState({...this.state, error: true, message: message});
-		//TODO: showing error in form
-	};
-	
-	_handleCheckbox = ()=> {
-		let checked = this.rememberme.checked;
-		this.setState({...this.state , checked: !checked},);
-		console.log('hey',checked)
-	};
-	
-	_handleClick = (e) => {
-		e.preventDefault();
-		const username = this.username.value;
-		const password = this.password.value;
-		if (username.length > 0 && password.length > 0) {
-			socket.emit(REST_REQUEST, {
-				method: "post",
-				url: url + "/api-token-auth/",
-				result: "TOKEN_Result",
-				data: {
-					username,
-					password
-				},
-			});
-		}
-	};
+  _handleError = (msg) => {
+    const message = 'نام کاربری یا گذرواژه صحیح نمی باشد';
+    this.setState({...this.state, error: true, message: message});
+    //TODO: showing error in form
+  };
 
-	render() {
-		const {error, message, rememberme} = this.state;
-		return (
-				<form action="#">
-					<div className="input-group-vertical mb-3">
-						<input
-								type="text"
-								name="username"
-								ref={username => {
-									this.username = username
-								}}
-								className="form-control form-control-lg"
-								placeholder={__('Username')}
-						/>
-						<input
-								type="password"
-								name="password"
-								ref={password => {
-									this.password = password
-								}}
-								className="form-control form-control-lg"
-								placeholder={__('Password')}
-						/>
-					</div>
-					<button onClick={this._handleClick} className="btn btn-primary btn-block btn-lg">{__('Login')}</button>
-					<ErrorMessage message={message} error={(error) ? error : ''}/>
-					<button className=" btn btn-link loginForm rememberme" onChange={this._handleCheckbox}>
-						<label htmlFor="rememberme" >
-							<input
-									id="rememberme"
-									type="checkbox"
-									checked={rememberme}
-									ref={rememberme => this.rememberme = rememberme}
-							/>
-							{__('Remember me')}
-						</label>
-					</button>
-					<button type="button" className="btn btn-link">
-						{__('Password recovery')}
-					</button>
-				</form>
-		)
-	}
+  _handleCheckbox = () => {
+    let checked = this.rememberme.checked;
+    this.setState({...this.state, rememberme: checked},);
+  };
+
+  _handleClick = (e) => {
+    e.preventDefault();
+    const username = this.username.value;
+    const password = this.password.value;
+    if (username.length > 0 && password.length > 0) {
+      socket.emit(REST_REQUEST, {
+        method: "post",
+        url: url + "/api-token-auth/",
+        result: "TOKEN_Result",
+        data: {
+          username,
+          password
+        },
+      });
+    }
+  };
+
+  render() {
+    const {error, message, rememberme} = this.state;
+    return (
+      <form action="#" className="sign-in-form">
+        <div className="input-group-vertical">
+          <input
+            type="text"
+            name="username"
+            ref={username => {
+              this.username = username
+            }}
+            className="form-control my-form-control-lg"
+            placeholder={__('Username')}
+          />
+          <input
+            type="password"
+            name="password"
+            ref={password => {
+              this.password = password
+            }}
+            className="form-control my-form-control-lg"
+            placeholder={__('Password')}
+          />
+        </div>
+        <button onClick={this._handleClick} className="btn btn-primary btn-block login-submit-button">{__('Login')}</button>
+        <ErrorMessage message={message} error={(error) ? error : ''}/>
+        <div className="remember-recovery">
+          <label htmlFor="rememberme">
+            <input
+              id="rememberme"
+              type="checkbox"
+              checked={rememberme}
+              ref={rememberme => this.rememberme = rememberme}
+              onChange={this._handleCheckbox}
+            />
+            {__('Remember me')}
+          </label>
+          <span className="btn btn-link recovery-button">
+            {__('Password recovery')}
+          </span>
+        </div>
+      </form>
+    )
+  }
 }
