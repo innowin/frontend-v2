653b9b17e4d1c0a8b00ba163ffff145cabe9217c
Pedram Rastegar <peddy.rast@gmail.com>
2018-05-26 20:21:35 +0430

    multi language is added to project

--- src/consts/PrivateRoute.js
+++ src/consts/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Route, Redirect} from 'react-router-dom'
-import {default as client} from './client'
+import client from './client'
 
 const PrivateRoute = ({component,...rest}) => (
-		<Route {...rest} render={(props)=> (
+		<Route {...rest} render={ props => (
 			client.isAuthenticated() ?
 					React.createElement(component , props)
 					:
