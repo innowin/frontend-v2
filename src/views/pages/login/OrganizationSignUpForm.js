c48f6f8aa30e8914e770a88b16f613dd0b53aa12
aliorooji <ali.orooji.ilia@gmail.com>
2018-06-06 15:32:03 +0430

    signupForm created by redux-form for person and organization. but the post data is not doed now. and used text input alternative to select input where at the moment.

--- src/views/pages/login/OrganizationSignUpForm.js
+++ src/views/pages/login/OrganizationSignUpForm.js
@@ -6,6 +6,10 @@ import { Field, reduxForm } from 'redux-form'
 // import {setID, setTOKEN, saveData} from "src/consts/data"
 // import {SOCKET as socket, REST_URL as url} from "src/consts/URLS"
 // import {BeatLoader} from "react-spinners"
+import {BeatLoader} from "react-spinners"
+import {validateSignup as validate} from './validations'
+import {SOCKET as socket, REST_URL as url} from "src/consts/URLS"
+import renderTextField from '../../common/inputs/reduxFormRenderTextField'
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -13,114 +17,75 @@ const asyncValidate = (values /*, dispatch */) => {
   return sleep(1000).then(() => {
     // simulate server latency
     if (['john', 'paul', 'george', 'ringo'].includes(values.username)) {
-      throw { username: 'That username is taken' }
+      throw { username: 'این نام کاربری قبلا استفاده شده است.' }
     }
   })
 }
 
-const _validateUsername = (username) => {
-  if (!/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/.test(username)) {
-    return 'Invalid username'
-  }
-  else if (username.length < 4) return 'Username length should be greater than 4'
-}
-
-const _validateEmail = (email) => {
-  if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) || email.length < 5) {
-    return 'Invalid email'
-  }
-};
-
-const _validateNationalCode = (nationalCode) => {
-  const code = +nationalCode;
-  if (Number.isInteger(code)){
-    if (nationalCode.length === 10) {
-      let checkNum = 0
-      for (let i = 0; i < 9; i++) checkNum += (nationalCode[i] * (10 - i))
-      checkNum = checkNum % 11
-      const code9 = nationalCode[9]
-      const condition = ((code9 == 1 && checkNum === 1) ||
-                         (code9 == 0 && checkNum === 0) ||
-                         (code9 == 11 - checkNum)
-                        )
-      console.log('code9 ', code9)
-      console.log('checkNum ', checkNum)
-      console.log('checkNum', checkNum)
-      if (condition) return ''
-
-      return 'Invalid national code'
-
-    } else return 'The national code should be 10 characters'
-
-  } return 'All of the national code characters should be number'
-}
-
-const validate = values => {
-  const errors = {}
-  const requiredFields = [
-    'username', 'email', 'official_name', 'national_code', 'country', 'province', 'password',
-    'passwordConfirm', 'city', 'organization_type', 'business_type']
-  const { username, email, national_code, password, passwordConfirm} = values
-
-  if (username) errors.username = _validateUsername(username)
-  if (email) errors.email = _validateEmail(email)
-  if (national_code) errors.national_code = _validateNationalCode(national_code)
-  
-  if (passwordConfirm && passwordConfirm !== password) errors.passwordConfirm = 'Two passwords do not match.'
-
-  requiredFields.forEach(field => {
-    if (!values[field]) errors[field] = 'This field is required!'
-  })
-  return errors
-}
-
-const renderTextField = ({
-  input,
-  label,
-  type,
-  meta: { touched, error, warning }
-}) => (
-  <div>
+const OrgFormPart1 = ({ handleSubmit, pristine, reset, submitting, onSubmit }) => (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <Field
+      name="username"
+      type="text"
+      component={renderTextField}
+      label="نام کاربری"
+    />
+    <Field name="email" type="email" component={renderTextField} label="ایمیل" />
+    <Field name="official_name" type="text" component={renderTextField} label="نام رسمی" />
+    <Field name="national_code" type="text" component={renderTextField} label="کد ملی" />
+    <Field name="organization_type" type="text" component={renderTextField} label="نوع شرکت" />
+    <Field name="business_type" type="text" component={renderTextField} label="نوع تجارت" />
     <div>
-      <input {...input} placeholder={label} type={type} />
-      {touched &&
-        ((error && <span>{error}</span>) ||
-          (warning && <span>{warning}</span>))}
+      <button
+        className="btn btn-primary btn-block login-submit-button mt-0 cursor-pointer"
+        disabled={submitting}>
+        {!submitting ? (__('Register')) : (
+          <BeatLoader color="#fff" size={10} margin="auto"/>
+        )}
+      </button>
     </div>
-  </div>
+  </form>
 )
-const OrganizationSignupForm = props => {
-  const { handleSubmit, pristine, reset, submitting } = props
-  return (
-    <form onSubmit={handleSubmit}>
-      <Field
-        name="username"
-        type="text"
-        component={renderTextField}
-        label="Username"
-      />
-      <Field name="email" type="email" component={renderTextField} label="Email" />
-      <Field name="official_name" type="text" component={renderTextField} label="Official_name" />
-      <Field name="national_code" type="text" component={renderTextField} label="National_code" />
-      <Field name="country" type="text" component={renderTextField} label="Country" />
-      <Field name="province" type="text" component={renderTextField} label="Province" />
-      <Field name="city" type="text" component={renderTextField} label="City" />
-      <Field name="organization_type" type="text" component={renderTextField} label="Organization type" />
-      <Field name="business_type" type="text" component={renderTextField} label="Business type" />
-      <Field name="password" type="password" component={renderTextField} label="Password" />
-      <Field name="passwordConfirm" type="password" component={renderTextField} label="Password Confirm" />
-      <div>
-        <button type="submit" disabled={submitting}>
-          ثبت نام
-        </button>
-        </div>
-    </form>
-  )
-}
-
-export default reduxForm({
-  form: 'organizationSignupForm',
+const OrgFormPart2 = ({ handleSubmit, pristine, reset, submitting, onSubmit }) => (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <Field name="country" type="text" component={renderTextField} label="کشور" />
+    <Field name="province" type="text" component={renderTextField} label="استان" />
+    <Field name="city" type="text" component={renderTextField} label="شهر" />
+    <Field name="password" type="password" component={renderTextField} label="رمز عبور" />
+    <Field name="passwordConfirm" type="password" component={renderTextField} label="تکرار رمز عبور" />
+    <div>
+      <button
+        className="btn btn-primary btn-block login-submit-button mt-0 cursor-pointer"
+        disabled={submitting}>
+        {!submitting ? (__('Register')) : (
+          <BeatLoader color="#fff" size={10} margin="auto"/>
+        )}
+      </button>
+    </div>
+  </form>
+)
+const OrgReduxFormPart1 = reduxForm({
+  form: 'orgSignupFormPart1',
   validate,
   asyncValidate,
   asyncBlurFields: ['username']
-})(OrganizationSignupForm)
+})(OrgFormPart1)
+
+const OrgReduxFormPart2 = reduxForm({
+  form: 'orgSignupFormPart2',
+  validate,
+})(OrgFormPart2)
+
+const OrganizationSignupForm = ({ onSubmitPart1, onSubmitPart2, formPart }) => (
+  <div className="organization-signup-form">  
+    <div className={formPart === 2 ? 'visible-part' : 'hidden-part' }>
+      <OrgReduxFormPart2 onSubmit={onSubmitPart2} />        
+    </div>
+    <div className={formPart === 1 ? 'visible-part' : 'hidden-part' }>
+      <OrgReduxFormPart1 onSubmit={onSubmitPart1} />
+    </div>
+  </div>
+)
+
+export default OrganizationSignupForm
+
--- src/views/pages/login/SignUpForm.js
+++ src/views/pages/login/SignUpForm.js
@@ -1,9 +1,12 @@
 /*global __*/
 import React, {Component} from 'react'
-import PersonSignUpForm from './PersonSignUpForm'
+import PersonSignupForm from './PersonSignUpForm'
 import OrganizationSignUpForm from './OrganizationSignUpForm'
 import { RadioButtonGroup } from '../../common/inputs/RadioButtonInput'
-
+import cookies from "browser-cookies"
+import {REST_REQUEST} from "src/consts/Events"
+import {setID, setTOKEN, saveData} from "src/consts/data"
+import {SOCKET as socket, REST_URL as url} from "src/consts/URLS"
 const USER_TYPES = {
   PERSON: 'person',
   ORGANIZATION: 'organization',
@@ -14,14 +17,58 @@ export class SignUpForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userType: USER_TYPES.PERSON
+      userType: USER_TYPES.PERSON,
+      orgFormPart: 0,
+      data: {}
     }
   }
-  _typeHandler = (value) => this.setState({ ...this.state, userType: value });
+  _typeHandler = (value) => {
+    let part = ((value === USER_TYPES.PERSON) && 0) || ((value === USER_TYPES.ORGANIZATION) && 1)
+    this.setState({ ...this.state, userType: value }, () => setTimeout(() => {
+      this._orgFormPartHandler(part)
+    }));
+  }
+  _orgFormPart1SubmitHandler = (values) => {
+    socket.emit(REST_REQUEST, {
+      method: "get",
+      url: `${url}/users/?username=${values.username}`,
+      result: "USERNAME_check",
+    }).then(res => console.log('my res is ', res))
+    this.setState({ ...this.state, orgFormPart: 2, data: { ...this.state.data, ...values} })
+  }
+
+  _orgFormPart2SubmitHandler = (values) => {
+    this.setState({ ...this.state, data: { ...this.state.data, ...values} })
+  }
+  
+  _handlePersonFormSubmit = (values) => {
+    // socket.on('USERNAME_check', (res) => {
+    //   console.log('res is ', res);
+    //   const {status, messages} = this.state;
+    //   if (res.length > 0) {
+    //     let message = __('Username exists');
+    //     this.setState({
+    //       ...this.state,
+    //       status: {...status, usernameS: 'error'},
+    //       messages: {...messages, usernameMB: message}
+    //     });
+    //     return false;
+    //   }
+    //   let message = __('Acceptable');
+    //   this.setState({
+    //     ...this.state,
+    //     status: {...status, usernameS: 'success'},
+    //     messages: {...messages, usernameMB: message}
+    //   });
+    // });
+    this.setState({ ...this.state, data: values}, () => console.log(this.state))
+  }
+
+  _orgFormPartHandler = part => this.setState({ ...this.state, orgFormPart: part})
 
   render() {
     const {RedirectToHome} = this.props;
-    const { userType } = this.state
+    const { userType, orgFormPart } = this.state
     const userTypeItems = [ // used for RadioButtonItems as items property.
       { value: USER_TYPES.PERSON, title: 'فرد' },
       { value: USER_TYPES.ORGANIZATION, title: 'سازمان' },
@@ -30,9 +77,9 @@ export class SignUpForm extends Component {
       <div className="signup-wrapper">
         <RadioButtonGroup selected={userType} handler={this._typeHandler} items={userTypeItems} name="userType" label="ثبت نام کننده" />
         {userType === USER_TYPES.PERSON ?
-          <PersonSignUpForm RedirectToHome={RedirectToHome} />
+          <PersonSignupForm onSubmit={this._handlePersonFormSubmit} RedirectToHome={RedirectToHome} />
           :
-          <OrganizationSignUpForm />
+          <OrganizationSignUpForm handlePart={this._orgFormPartHandler} formPart={orgFormPart} onSubmitPart2={this._orgFormPart2SubmitHandler} onSubmitPart1={this._orgFormPart1SubmitHandler} />
         }
       </div>
     )
