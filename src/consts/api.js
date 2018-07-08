56d72d0cb37f4cae5761bb62b85dd6ce2cefdeea
mohsen <mohsen.workman@gmail.com>
2018-07-08 20:05:07 +0430

    fix handleLogin by saga

--- src/consts/api.js
+++ src/consts/api.js
@@ -1,4 +1,4 @@
-import {SOCKET,REST_URL} from "./URLS"
+import {SOCKET, REST_URL} from "./URLS"
 import {REST_REQUEST} from "./Events"
 import client from './client'
 import {eventChannel} from 'redux-saga'
@@ -6,63 +6,63 @@ import {eventChannel} from 'redux-saga'
 let token = client.getToken()
 
 const createSocketChannel = (resultName) => {
-	return eventChannel(emit => {
-		const resultHandler = res => { 
-			if(res.detail){
-				emit(new Error(res.detail))
-				return;
-			}
-			emit(res)
-		}
-		SOCKET.on(resultName,resultHandler)
-		return () =>	SOCKET.off(resultName,resultHandler)
-	})
+  return eventChannel(emit => {
+    const resultHandler = res => {
+      if (res.detail) {
+        emit(new Error(res.detail))
+        return;
+      }
+      emit(res)
+    }
+    SOCKET.on(resultName, resultHandler)
+    return () => SOCKET.off(resultName, resultHandler)
+  })
 }
 
-const get = (url , resultName, query = "") => {
-	let token = client.getToken()
-	SOCKET.emit(REST_REQUEST, {
-		method: 'get',
-		url: REST_URL + '/' + url+'/'+ query,
-		result: resultName,
-		token
-	})
+const get = (url, resultName, query = "") => {
+  let token = client.getToken()
+  SOCKET.emit(REST_REQUEST, {
+    method: 'get',
+    url: REST_URL + '/' + url + '/' + query,
+    result: resultName,
+    token
+  })
 }
 
-const patch = (url , resultName ,data,query = "") => {
-	SOCKET.emit(REST_REQUEST, {
-		method: 'patch',
-		url: REST_URL+'/'+url+'/'+ query+'/',
-		result: resultName,
-		data,
-		token
-	})
+const patch = (url, resultName, data, query = "") => {
+  SOCKET.emit(REST_REQUEST, {
+    method: 'patch',
+    url: REST_URL + '/' + url + '/' + query + '/',
+    result: resultName,
+    data,
+    token
+  })
 }
 
-const del = (url , resultName , data, query = "") => {
-	SOCKET.emit(REST_REQUEST, {
-		method: 'del',
-		url: REST_URL+'/'+url+'/'+ query+'/',
-		result: resultName,
-		data,
-		token
-	})
+const del = (url, resultName, data, query = "") => {
+  SOCKET.emit(REST_REQUEST, {
+    method: 'del',
+    url: REST_URL + '/' + url + '/' + query + '/',
+    result: resultName,
+    data,
+    token
+  })
 }
 
-const post = (url , resultName ,data,query = "") => {
-	SOCKET.emit(REST_REQUEST, {
-		method: 'post',
-		url: REST_URL+'/'+url+'/'+ query,
-		result: resultName,
-		data,
-		token
-	})
+const post = (url, resultName, data, query = "") => {
+  SOCKET.emit(REST_REQUEST, {
+    method: 'post',
+    url: REST_URL + '/' + url + '/' + query,
+    result: resultName,
+    data,
+    token
+  })
 }
 const api = {
-	createSocketChannel,
-	get,
-	post,
-	patch,
-	del
+  createSocketChannel,
+  get,
+  post,
+  patch,
+  del
 }
 export default api
--- src/redux/sagas/auth/authSaga.js
+++ src/redux/sagas/auth/authSaga.js
@@ -1,7 +1,7 @@
 import types from 'src/redux/actions/actionTypes'
 import urls from 'src/consts/URLS'
 import {delay} from 'redux-saga'
-import {put,take, fork, apply,call, takeEvery} from 'redux-saga/effects'
+import {put, take, fork, apply, call, takeEvery} from 'redux-saga/effects'
 import api from 'src/consts/api'
 import client from 'src/consts/client'
 import results from 'src/consts/resultName'
@@ -9,57 +9,65 @@ import results from 'src/consts/resultName'
 /**********    %% WORKERS %%    **********/
 
 //1 - prerequisite - sending requests
-function* sendRequest(username,password) {
-	yield apply({}, api.post, [urls.SIGN_IN, results.SIGN_IN,{username,password}])
+function* signInRequest(username, password) {
+  yield apply({}, api.post, [urls.SIGN_IN, results.SIGN_IN, {username, password}])
 }
+
+function* getOrganRequest(username) {
+  yield apply({}, api.get, [urls.GET_ORGANIZATION, results.GET_ORGANIZATION, `?username=${username}`])
+}
+
 //1 - sign in worker
-export function* signIn (action) {
-	const {payload} = action
-	const {username ,password,remember} = payload
-	const socketChannel = yield call(api.createSocketChannel, results.SIGN_IN)
-	try {
-		yield fork(sendRequest , username,password )
-		while (true) {
-			const data = yield take(socketChannel)
-			let identityType = data.identity.identity_user !== null
-			if (identityType) {
-				yield client.saveData(data.user.id , data.identity.id,'person',remember)
-			} else {
-				yield client.saveData(data.user.id , data.identity.id, 'organization',remember)
-			}
-			if (remember) {
-				yield client.setTokenLS(data.token)
-			} else {
-				yield client.setSessionLS(data.token)
-			}
-			yield delay(500)
-			yield put({ type: types.SIGN_IN_SUCCESS, payload:{data , rememberMe:remember} })
-		}
-	} catch (e) {
-		const {message} = e
-		yield put({type:types.SIGN_IN_ERROR, payload:{type:types.ERRORS.SIGN_IN,message}})
-	} finally {
-		socketChannel.close()
-	}
+export function* signIn(action) {
+  const {payload} = action
+  const {username, password, remember} = payload
+  const socketChannel = yield call(api.createSocketChannel, results.SIGN_IN)
+  try {
+    yield fork(signInRequest, username, password)
+    const data = yield take(socketChannel)
+    let identityType = data.identity.identity_user !== null
+    if (identityType) {
+      yield client.saveData(data.user.id, data.identity.id, 'person', remember)
+    } else {
+      const organId = yield* call(getOrganRequest, data.user.username)
+      yield client.saveData(data.user.id, data.identity.id, 'organization', remember)
+    }
+    if (remember) {
+      yield client.setTokenLS(data.token)
+    } else {
+      yield client.setSessionLS(data.token)
+    }
+    yield delay(500)
+    yield put({type: types.SIGN_IN_SUCCESS, payload: {data, rememberMe: remember}})
+
+  }
+  catch (e) {
+    const {message} = e
+    yield put({type: types.SIGN_IN_ERROR, payload: {type: types.ERRORS.SIGN_IN, message}})
+  }
+  finally {
+    socketChannel.close()
+  }
 }
+
 //2 - Sign Out worker
-export function* signOut () {
-	yield call(client.clearToken)
-	yield put({type:types.SIGN_OUT_FINISHED, payload:{}})
+export function* signOut() {
+  yield call(client.clearToken)
+  yield put({type: types.SIGN_OUT_FINISHED, payload: {}})
 }
 
 /**********    %% WATCHERS %%    **********/
 //1 - sign In
 export function* watchLSignIn() {
-	yield takeEvery(types.SIGN_IN,signIn)
+  yield takeEvery(types.SIGN_IN, signIn)
 }
 
 //2 - sign out
 export function* watchLSignOut() {
-	yield takeEvery(types.SIGN_OUT,signOut)
+  yield takeEvery(types.SIGN_OUT, signOut)
 }
 
 //3 - sign in error
 export function* watchLSignInError() {
-	yield takeEvery(types.SIGN_IN_ERROR,signOut)
+  yield takeEvery(types.SIGN_IN_ERROR, signOut)
 }
