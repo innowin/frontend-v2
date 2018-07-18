89ab4d32bf3cc14ff35e60131059682d915078b7
Pedram Rastegar <peddy.rast@gmail.com>
2018-07-18 11:00:45 +0430

    persistence added to redux data

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import configureStore,{runSaga , history} from "./redux/store/configureStore"
+import configureStore, {runSaga, history} from "./redux/store/configureStore"
 import registerServiceWorker from "./registerServiceWorker"
 import {ConnectedRouter} from "react-router-redux"
 import {IntlProvider} from "react-intl-redux"
 import {Provider} from "react-redux"
 import {withRouter} from "react-router-dom"
+import {persistStore} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
 
 const WrappedApp = withRouter(App)
 const store = configureStore()
+export const persistor = persistStore(store)
 runSaga()
 
 ReactDOM.render(
-    <Provider store={store}>
-			<IntlProvider>
-				<ConnectedRouter history={history}>
-					<WrappedApp/>
-				</ConnectedRouter>
-			</IntlProvider>
-    </Provider>
-
-    , document.getElementById('root'))
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<IntlProvider>
+					<ConnectedRouter history={history}>
+						<WrappedApp/>
+					</ConnectedRouter>
+				</IntlProvider>
+			</PersistGate>
+		</Provider>
+		
+		, document.getElementById('root'))
 registerServiceWorker()
