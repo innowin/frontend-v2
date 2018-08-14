d96a75309b68c5a280fcbf675a686fe2a80e50b0
Mohammad <mohammad.hooshdar@yahoo.com>
2018-08-14 16:44:01 +0430

    #125 tabs style fixed, category title divider added

--- src/views/User.js
+++ src/views/User.js
@@ -7,7 +7,7 @@ import WorkExperiences from "./user/workExperience/index"
 import Certificates from "./user/certificates/index"
 import ChatBar from "src/views/bars/ChatBar"
 import Posts from "src/views/common/post/index"
-import PrivateRoute from "../consts/PrivateRoute"
+  import PrivateRoute from "../consts/PrivateRoute"
 import Sidebar from "src/views/bars/SideBar"
 import Skills from "./user/skills/index"
 import Social from "src/views/user/social/index"
@@ -32,6 +32,7 @@ class User extends Component<PropTypes> {
   }
 
   render() {
+    {/* TODO: fake*/}
     const {match, handleSignOut} = this.props
     const {path, url, params} = match
     const userId: number = +params.id
@@ -51,23 +52,26 @@ class User extends Component<PropTypes> {
           </div>
           <div className="col-6 -content-wrapper">
             <Tabs>
-              <NavLink className="-tab" to={`${url}/basicInformation`}
-                       activeClassName="-active">{userInfoIcon}</NavLink>
               <NavLink className="-tab" to={`${url}/Posts`} activeClassName="-active">{postIcon}</NavLink>
-              <NavLink className="-tab" to={`${url}/WorkExperiences`} activeClassName="-active">{workExperienceIcon}</NavLink>
+              <NavLink className="-tab" to={`${url}/basicInformation`}
+                activeClassName="-active">{userInfoIcon}</NavLink>
+              {/* TODO: mohammad add contributions and its route*/}
               <NavLink className="-tab" to={`${url}/SocialConnections`} activeClassName="-active">
                 <SocialIcon/>
               </NavLink>
+              {/* TODO: mohammad add education and its route*/}
+              {/* FixMe: mohammad workExperiences and skills must be join to workExperiences and join their routes*/}
+              <NavLink className="-tab" to={`${url}/WorkExperiences`} activeClassName="-active">{workExperienceIcon}</NavLink>
               <NavLink className="-tab" to={`${url}/Skills`} activeClassName="-active"><SkillIcon/></NavLink>
               <NavLink className="-tab" to={`${url}/Certificates`}
                        activeClassName="-active"><CertificateIcon/></NavLink>
             </Tabs>
             <Switch>
-              <Redirect exact from={`${url}/`} to={`${url}/basicInformation`}/>
-              <PrivateRoute path={`${path}/basicInformation`} component={UserBasicInformation} userId={userId}/>
+              <Redirect exact from={`${url}/`} to={`${url}/Posts`}/>
               <PrivateRoute path={`${path}/Posts`} component={Posts} id={userId} identityType='user'/>
-              <PrivateRoute path={`${path}/WorkExperiences`} component={WorkExperiences} userId={userId}/>
+              <PrivateRoute path={`${path}/basicInformation`} component={UserBasicInformation} userId={userId}/>
               <PrivateRoute path={`${path}/SocialConnections`} component={Social} userId={userId}/>
+              <PrivateRoute path={`${path}/WorkExperiences`} component={WorkExperiences} userId={userId}/>
               <PrivateRoute path={`${path}/Skills`} component={Skills} userId={userId}/>
               <PrivateRoute path={`${path}/Certificates`} component={Certificates} userId={userId}/>
             </Switch>
--- src/views/common/cards/Frames.js
+++ src/views/common/cards/Frames.js
@@ -9,204 +9,205 @@ import {loadingCard} from "./LoadingCard"
 
 type div = React.Element<'div'>
 type PropsTabs = {
-    children?: React.Node
+  children?: React.Node
 }
 
 export const Tabs = (props: PropsTabs): div => {
-    return (
-        <div className="mt-4 mb-4">
-            <div className="-tabs">
-                {props.children}
-            </div>
+  return (
+      <div className="tab-container">
+        <div className="-tabs">
+          {props.children}
         </div>
-    )
+      </div>
+  )
 }
 
 type PropsItemWrapper = {
-    children?: React.Node,
-    icon: React.Element<any>,
-    className?: string
+  children?: React.Node,
+  icon: React.Element<any>,
+  className?: string
 }
 
 export const ItemWrapper = (props: PropsItemWrapper): div => {
-    const {icon, children} = props
-    const className = props.className || ''
-    return (
-        <div className={"-itemWrapper " + className}>
-            <div className="-item-icon">{icon}</div>
-            <div className="-item-content">
-                {children}
-            </div>
+  const {icon, children} = props
+  const className = props.className || ''
+  return (
+      <div className={"-itemWrapper " + className}>
+        <div className="-item-icon">{icon}</div>
+        <div className="-item-content">
+          {children}
         </div>
-    )
+      </div>
+  )
 }
 ItemWrapper.propTypes = {
-    icon: PropTypes.node,
-    className: PropTypes.string
+  icon: PropTypes.node,
+  className: PropTypes.string
 }
 
 
 type PropsListGroup = {
-    children?: React.Node
+  children?: React.Node
 }
 
 export const ListGroup = (props: PropsListGroup): div => {
-    return (
-        <div className="list-group list-group-flush">
-            {props.children}
-        </div>
-    )
+  return (
+      <div className="list-group list-group-flush">
+        {props.children}
+      </div>
+  )
 }
 
 type PropsItemHeader = {
-    showEdit?: Function,
-    title: string
+  showEdit?: Function,
+  title: string
 }
 
 export const ItemHeader = (props: PropsItemHeader): div => {
-    const {showEdit} = props
-    return (
-        <div className="-item-header">
-            <div className="-item-title">{props.title}</div>
-            {/*<div className="-item-edit-btn"> commented by ali*/ }
-                {/*{*/}
-                    {/*(showEdit !== null) ?*/}
-                        {/*<div onClick={showEdit}><EditIcon/></div> : <span/>*/}
-                {/*}*/}
-            {/*</div>*/}
-        </div>
-    )
+  const {showEdit} = props
+  return (
+      <div className="-item-header">
+        <div className="-item-title">{props.title}</div>
+        {/*<div className="-item-edit-btn"> commented by ali*/}
+        {/*{*/}
+        {/*(showEdit !== null) ?*/}
+        {/*<div onClick={showEdit}><EditIcon/></div> : <span/>*/}
+        {/*}*/}
+        {/*</div>*/}
+      </div>
+  )
 }
 ItemHeader.propTypes = {
-    title: PropTypes.node,
-    showEdit: PropTypes.func
+  title: PropTypes.node,
+  showEdit: PropTypes.func
 }
 
 
 type PropsFrameCard = {
-    children?: React.Node,
-    className?: string
+  children?: React.Node,
+  className?: string
 }
 
 export const FrameCard = (props: PropsFrameCard): div => {
-    const className = props.className || ''
-    return (
-        <div className={cx("-frameCard\u0020" + className)}>
-            {props.children}
-        </div>
-    )
+  const className = props.className || ''
+  return (
+      <div className={cx("-frameCard\u0020" + className)}>
+        {props.children}
+      </div>
+  )
 }
 FrameCard.propTypes = {
-    className: PropTypes.string
+  className: PropTypes.string
 }
 
 type PropsCategoryTitle = {
-    title: string,
-    createForm?: boolean,
-    showCreateForm?: Function,
-    showEditBtn?: boolean,
-    showEditHandler?: Function
+  title: string,
+  createForm?: boolean,
+  showCreateForm?: Function,
+  showEditBtn?: boolean,
+  showEditHandler?: Function
 }
 
 export const CategoryTitle = ({
-                                  title, createForm = true,
-                                  showEditBtn = false,
-                                  showCreateForm = () => false,
-                                  showEditHandler = () => 0
+                                title, createForm = true,
+                                showEditBtn = true,
+                                showCreateForm = () => false,
+                                showEditHandler = () => 0
                               }: PropsCategoryTitle): div => {
-    return (
+  return (
+      <div className="category-title-container">
         <div className="-categoryTitle">
-            <span>{title}</span>
-            {
-                !createForm &&
-                <button className="btn btn-sm btn-outline-success" onClick={showCreateForm}>
-                    <FontAwesome name="plus"/>
-                </button>
-            }
-            {showEditBtn ?
-                <div className="edit-btn-wrapper" onClick={showEditHandler}><EditIcon className="edit-btn"/></div>
-                :
-                ''
-            }
+          <span>{title}</span>
+          {
+            !createForm &&
+            <button className="btn btn-sm btn-outline-success" onClick={showCreateForm}>
+              <FontAwesome name="plus"/>
+            </button>
+          }
+          {showEditBtn &&
+            <div className="edit-btn-wrapper" onClick={showEditHandler}><EditIcon className="edit-btn"/></div>
+          }
         </div>
-    )
+        <div className="category-divider"></div>
+      </div>
+  )
 }
 CategoryTitle.propTypes = {
-    title: PropTypes.node,
-    createForm: PropTypes.bool,
-    showCreateForm: PropTypes.func,
+  title: PropTypes.node,
+  createForm: PropTypes.bool,
+  showCreateForm: PropTypes.func,
 }
 
 
 type PropsFieldLabel = {
-    label: string
+  label: string
 }
 
 export const FieldLabel = (props: PropsFieldLabel): div => {
-    return (
-        <div className="col-5">
-            {props.label}
-        </div>
-    )
+  return (
+      <div className="col-5">
+        {props.label}
+      </div>
+  )
 }
 FieldLabel.propTypes = {
-    label: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
 }
 
 
 type PropsFieldValue = {
-    value: string | React.Element<'span'>
+  value: string | React.Element<'span'>
 }
 
 export const FieldValue = (props: PropsFieldValue) => {
-    return (
-        <div className="col-7 font-weight-bold break-word">
-            {props.value}
-        </div>
-    )
+  return (
+      <div className="col-7 font-weight-bold break-word">
+        {props.value}
+      </div>
+  )
 }
 FieldValue.propTypes = {
-    value: PropTypes.any
+  value: PropTypes.any
 }
 
 type PropsField = {
-    children?: React.Node
+  children?: React.Node
 }
 export const Field = (props: PropsField): div => {
-    return (
-        <div className="row col-form-label">
-            {props.children}
-        </div>
-    )
+  return (
+      <div className="row col-form-label">
+        {props.children}
+      </div>
+  )
 }
 
 type PropsVerifyWrapper = {
-    error: ?(boolean | string),
-    retry?: Function,
-    children?: React.Node,
-    className?: string,
-    isLoading: boolean
+  error: ?(boolean | string),
+  retry?: Function,
+  children?: React.Node,
+  className?: string,
+  isLoading: boolean
 }
 
 export const VerifyWrapper = ({error, retry = () => alert("retry"), ...props}: PropsVerifyWrapper): div => {
-    const {isLoading, children, className} = props
-    if (!isLoading) {
-        if (!error) {
-            return (
-                <div className={className}>
-                    {children}
-                </div>
-            )
-        }
-        // TODO mohsen #32 handel error message and retry in ErrorCard
-        return <ErrorCard retry={retry} header={error}/>
+  const {isLoading, children, className} = props
+  if (!isLoading) {
+    if (!error) {
+      return (
+          <div className={className}>
+            {children}
+          </div>
+      )
     }
-    return loadingCard()
+    // TODO mohsen #32 handel error message and retry in ErrorCard
+    return <ErrorCard retry={retry} header={error}/>
+  }
+  return loadingCard()
 }
 VerifyWrapper.propTypes = {
-    isLoading: PropTypes.bool.isRequired,
-    error: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-    retry: PropTypes.func,
-    className: PropTypes.string
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  retry: PropTypes.func,
+  className: PropTypes.string
 }
 
