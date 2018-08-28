57df6e1e2b9b6febfb818851d33332e01d7105ce
mohsen <mohsen.workman@gmail.com>
2018-08-28 11:38:29 +0430

    fix create post action

--- src/redux/sagas/common/post/createPost.js
+++ src/redux/sagas/common/post/createPost.js
@@ -6,13 +6,12 @@ import {put, take, fork, call} from "redux-saga/effects"
 
 export function* createPost(action) {
 
-  const {formValues, userId, resolveFunc:hideForm} = action.payload
+  const {formValues, userId} = action.payload
   const socketChannel = yield call(api.createSocketChannel, results.COMMON.POST.CREATE_POST)
   try {
     yield fork(api.post, urls.COMMON.POST.CREATE_POST, results.COMMON.POST.CREATE_POST, formValues)
     const data = yield take(socketChannel)
     yield put({type: types.SUCCESS.COMMON.CREATE_POST , payload:{data, userId}})
-    hideForm()
   } catch (error) {
     const {message} = error
     yield put({
