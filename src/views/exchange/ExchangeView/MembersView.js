8df1af0536231c8a3121e2a93d73c578f227476c
Abolfazl Miri <miri1888@gmail.com>
2018-10-13 16:22:10 +0330

    members in exchange done, follow request done #130

--- src/views/exchange/ExchangeView/MembersView.js
+++ src/views/exchange/ExchangeView/MembersView.js
@@ -6,13 +6,15 @@ import connect from "react-redux/es/connect/connect"
 import Contacts from "../../../images/common/contacts_svg"
 import {VerifyWrapper} from "../../common/cards/Frames"
 import {ClipLoader} from "react-spinners"
+import DefaultUserIcon from "../../../images/defaults/defaultUser_svg"
 
 class MembersView extends Component {
   constructor(props) {
     super(props)
     this.changeViewType = this.changeViewType.bind(this)
     this.getMembers = this.getMembers.bind(this)
-    this.state = {initialMembers: [], viewType: "member-square"}
+    this.setAllMembers = this.setAllMembers.bind(this)
+    this.state = {initialMembers: [], viewType: "member-square", moreMembers: false}
   }
 
   changeViewType() {
@@ -21,13 +23,58 @@ class MembersView extends Component {
     } else this.setState({...this.state, viewType: "member-square"})
   }
 
-  getMembers(memberId, memberType) {
+  getMembers(memberId, memberType, index) {
     let {profiles} = this.props
     if (memberType === "USER")
-      if (profiles[memberId].profile.content.profile_user)
-        return <div className={this.state.viewType}>{profiles[memberId].profile.content.profile_user.username}</div>
+      if (profiles[memberId].profile.content.profile_user) {
+        console.log(profiles[memberId].profile.content)
+        return <div key={index}
+                    className={this.state.viewType}>
+          <div className={"member-follow"}><span className={"member-follow-plus"}> + </span></div>
+          <div className={"member-picture-container"}>
+            {profiles[memberId].profile.content.profile_media !== null ? <img alt={"تصویر پروفایل"}
+                                                                              src={profiles[memberId].profile.content.profile_media.file}
+                                                                              width={"55px"} height={"55px"}
+                                                                              className={"member-picture"}/>
+                : <DefaultUserIcon
+                    height={"55px"} width={"55px"} className={"member-picture"}/>}
+          </div>
+          <div className={"member-info-container"}>
+            <div className={"member-name"}>{
+              profiles[memberId].profile.content.profile_user.first_name ||
+              profiles[memberId].profile.content.profile_user.last_name ?
+                  profiles[memberId].profile.content.profile_user.first_name + " " + profiles[memberId].profile.content.profile_user.last_name :
+                  profiles[memberId].profile.content.profile_user.username
+            }</div>
+            <div className={"member-description"}>{
+              profiles[memberId].profile.content.description
+            }</div>
+          </div>
+        </div>
+      }
       else return <div className={"member-loading"}>
         <ClipLoader color={"#cbcbcb"} size={90}/></div>
+  }
+
+  setAllMembers() {
+    let {exchangeUsers, exchangeId, actions} = this.props
+    let {getUser} = actions
+    let temp = []
+    if (exchangeUsers) {
+      if (exchangeUsers[exchangeId]) {
+        for (let i = 0; i < exchangeUsers[exchangeId].length; i++) {
+          if (exchangeUsers[exchangeId][i]) {
+            if (exchangeUsers[exchangeId][i].type === "USER") {
+              getUser(parseInt(exchangeUsers[exchangeId][i].id, 10))
+            } else {
+              // Get organization profile
+            }
+            temp.push(exchangeUsers[exchangeId][i])
+          }
+        }
+        this.setState({...this.state, initialMembers: temp.slice(), moreMembers: true})
+      }
+    }
   }
 
   componentDidMount() {
@@ -75,7 +122,7 @@ class MembersView extends Component {
   }
 
   render() {
-    let {initialMembers} = this.state
+    let {initialMembers, moreMembers} = this.state
     return (
         <div className={"members-frame"}>
           <div className={"members-header-right"}>
@@ -86,9 +133,12 @@ class MembersView extends Component {
             SWITCH
           </div>
           <div className={"members-body"}>
-            {initialMembers.length > 0 ? initialMembers.map((p) => {
-              return this.getMembers(p.id, p.type)
+            {initialMembers.length > 0 ? initialMembers.map((p, index) => {
+              return this.getMembers(p.id, p.type, index)
             }) : <VerifyWrapper isLoading={true} error={false}/>}
+            {!moreMembers ? <div className={"members-more"} onClick={this.setAllMembers}>
+              بارگذاری بیشتر
+            </div> : <div></div>}
           </div>
         </div>
     )
