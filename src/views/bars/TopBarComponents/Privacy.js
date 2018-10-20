812008229dd53e39e95a11c83c4819e4855380fd
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2018-10-20 17:37:03 +0330

    privacy menu updated #138

--- src/views/bars/TopBarComponents/Privacy.js
+++ src/views/bars/TopBarComponents/Privacy.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
 import {bindActionCreators} from "redux"
-import AuthActions from "../../../redux/actions/authActions"
-import {routerActions} from "react-router-redux"
-import FileActions from "../../../redux/actions/commonActions/fileActions"
 import connect from "react-redux/es/connect/connect"
 
 class Privacy extends Component {
@@ -11,9 +8,8 @@ class Privacy extends Component {
         <div>
           <div className='settingModal-menu-privacy-who-follow-container'>
             <div className='settingModal-menu-privacy-header'>{this.props.translate["Manage Followers"]}</div>
-            <div
-                className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Should Can Follow You?"]}</div>
 
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Should Can Follow You?"]}</div>
             <div className='settingModal-menu-privacy-check-container'>
               <form>
                 <label className='settingModal-menu-privacy-checkbox'>
@@ -26,6 +22,206 @@ class Privacy extends Component {
                   <input type="radio" name="kind"/>
                   <span className='checkmark'></span>
                   {this.props.translate["Only Accepted Requests"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Base Info?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Activity?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Work Experiences?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Badges?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Certificates?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Followers?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Followings?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
+                </label>
+              </form>
+            </div>
+
+            <br/>
+
+            <div className='settingModal-menu-privacy-who-follow'>{this.props.translate["Who Can Read Exchanges?"]}</div>
+            <div className='settingModal-menu-privacy-check-container'>
+              <form>
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["All People"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Only Followers"]}
+                </label>
+
+                <label className='settingModal-menu-privacy-checkbox'>
+                  <input type="radio" name="kind"/>
+                  <span className='checkmark'></span>
+                  {this.props.translate["Nobody"]}
                 </label>
               </form>
             </div>
