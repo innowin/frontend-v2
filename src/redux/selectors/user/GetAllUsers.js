ac9bb5044720abc229207452010926519c319c89
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2018-11-07 22:25:16 +0330

    user explore view with real users provided

---
+++ src/redux/selectors/user/GetAllUsers.js
@@ -0,0 +1,28 @@
+import {createSelector} from 'reselect'
+
+const getAllUsers = (state) => {
+  let allUsers = {...state.users.allUsers}
+  delete allUsers[state.auth.client.organization !== null ? state.auth.client.organization.id : state.auth.client.user.id]
+  // let searched = state.exchanges.searchByWord ? state.exchanges.searchByWord : []
+  // if (searched.length > 0) {
+  //   if ((searched[0] !== -1)) {
+  //     let users = {}
+  //     Object.values(searched).forEach(p => {
+  //       users[p] = allUsers[p]
+  //     })
+  //     return users
+  //   }
+  //   else
+  //   {
+  //     return []
+  //   }
+  // }
+  // else {
+    return allUsers
+  // }
+}
+
+export const getUsers = createSelector(
+    getAllUsers,
+    user => user
+)
