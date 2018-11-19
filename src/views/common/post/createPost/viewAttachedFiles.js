287eb4c33550c740870e1cda23de1fab019b3da3
mohsen <mohsen.workman@gmail.com>
2018-11-19 20:48:16 +0330

    handle error attah file in create post

--- src/views/common/post/createPost/viewAttachedFiles.js
+++ src/views/common/post/createPost/viewAttachedFiles.js
@@ -2,48 +2,41 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const ViewAttachedFiles = (props) => {
-  const {
-    postPictures, errorAttachPicture, postMedia, errorAttachMedia, postFile, errorAttachFile,
-    deletePicture, deleteMedia, deleteFile
-  } = props
+  const {postPictures, postMedia, postFile, deletePicture, deleteMedia, deleteFile} = props
   return (
-      <div className="post-attached-pictures">
-        {
-          postPictures.length > 0 ?
-              <div className="pictures-section">
-                {
-                  postPictures.map((fileString, i) => (
-                          <div>
-                            <span onClick={() => deletePicture(i)} className='remove-post-picture pulse'>x</span>
-                            <img src={fileString} alt="imagePreview"/>
-                          </div>
-                      )
-                  )
-                }
-              </div> : ''
-        }
-        {
-          errorAttachPicture ? <div className="form-control-feedback">{errorAttachPicture}</div> : ''
-        }
-        {
-          (postMedia) ? (
-              <div className="media-section">
-                <span onClick={deleteMedia} className='remove-post-picture pulse'>x</span>
-                <video width="100%" height="200px" controls poster="">
-                  <source src={postMedia} type="video/mp4"/>
-                </video>
-              </div>
-          ) : (errorAttachMedia ? <div className="form-control-feedback">{errorAttachMedia}</div> : '')
-
-        }
-        {
-          (postFile) ? (
-              <div className="file-section">
-                <span onClick={deleteFile} className='remove-post-picture pulse'>x</span>
-              </div>
-          ) : (errorAttachFile ? <div className="form-control-feedback">{errorAttachFile}</div> : '')
-        }
-      </div>
+    <div className="post-attached-pictures">
+      {
+        postPictures.length > 0 ?
+          <div className="pictures-section">
+            {
+              postPictures.map((fileString, i) => (
+                  <div key={i + "pictures-section"}>
+                    <span onClick={() => deletePicture(i)} className='remove-post-picture pulse'>x</span>
+                    <img src={fileString} alt="imagePreview"/>
+                  </div>
+                )
+              )
+            }
+          </div> : ''
+      }
+      {
+        (postMedia) ? (
+          <div className="media-section">
+            <span onClick={deleteMedia} className='remove-post-picture pulse'>x</span>
+            <video width="100%" height="200px" controls poster="">
+              <source src={postMedia} type="video/mp4"/>
+            </video>
+          </div>
+        ) : ''
+      }
+      {
+        (postFile) ? (
+          <div className="file-section">
+            <span onClick={deleteFile} className='remove-post-picture pulse'>x</span>
+          </div>
+        ) : ''
+      }
+    </div>
   )
 }
 
