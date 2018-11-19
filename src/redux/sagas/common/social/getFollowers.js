48776bc95c91e9a0567c74807a4790cc6bbb1284
Mohammad <mohammad.hooshdar@yahoo.com>
2018-11-19 12:13:01 +0330

    boolean for not get profile followers added

--- src/redux/sagas/common/social/getFollowers.js
+++ src/redux/sagas/common/social/getFollowers.js
@@ -5,19 +5,21 @@ import types from 'src/redux/actions/types'
 import {put, take, fork, call} from "redux-saga/effects"
 
 export function* getFollowers(action) {
-  const {followOwnerIdentity, followOwnerType, followOwnerId} = action.payload
+  const {notProfile, followOwnerIdentity, followOwnerType, followOwnerId} = action.payload
   const socketChannel = yield call(api.createSocketChannel, results.COMMON.SOCIAL.GET_FOLLOWERS)
   try {
     yield fork(api.get, urls.COMMON.SOCIAL.FOLLOW, results.COMMON.SOCIAL.GET_FOLLOWERS, `?follow_followed=${followOwnerIdentity}`)
     const data = yield take(socketChannel)
     yield put({type: types.SUCCESS.COMMON.SOCIAL.GET_FOLLOWERS , payload:{data, followOwnerId, followOwnerIdentity, followOwnerType}})
-    for (let follow of data) {
-      const follower = follow.follow_follower
-      if (follower.identity_user) {
-        yield put({type: types.USER.GET_PROFILE_BY_USER_ID, payload:{userId: follower.identity_user}})
-      }
-      else {
-        yield put({type: types.ORG.GET_ORGANIZATION, payload:{organizationId: follower.identity_organization}})
+    if (!notProfile) {
+      for (let follow of data) {
+        const follower = follow.follow_follower
+        if (follower.identity_user) {
+          yield put({type: types.USER.GET_PROFILE_BY_USER_ID, payload: {userId: follower.identity_user}})
+        }
+        else {
+          yield put({type: types.ORG.GET_ORGANIZATION, payload: {organizationId: follower.identity_organization}})
+        }
       }
     }
   } catch (error) {
