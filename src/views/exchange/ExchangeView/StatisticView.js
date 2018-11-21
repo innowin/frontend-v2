9853854327a295058459d919052266a7db108d84
Abolfazl Miri <miri1888@gmail.com>
2018-11-21 09:47:28 +0330

    statistics views in develop

---
+++ src/views/exchange/ExchangeView/StatisticView.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+class StatisticView extends Component
+{
+  render()
+  {
+    return (
+        <div className={"statistics-container"}>
+          <div className={"statistics-bodies"}>
+            Developing...
+          </div>
+          <div className={"statistics-bodies"}>
+            Developing...
+          </div>
+        </div>
+    )
+  }
+}
+
+export default StatisticView
