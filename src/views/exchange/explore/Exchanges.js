3a34cca3ccb8bf7a1967380d3189a6694daae02d
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2018-12-05 12:26:30 +0330

    fix some exchange new structure errors

--- src/views/exchange/explore/Exchanges.js
+++ src/views/exchange/explore/Exchanges.js
@@ -13,21 +13,27 @@ const loadingArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const Exchanges = (props: appProps) => {
   let {exchanges, justFollowing} = props
 
-  console.log('ex : ',exchanges)
+  console.log('ex : ', exchanges)
 
   exchanges = Object.values(exchanges).filter(exchange =>
-      exchange.exchange.content.id
+      exchange.exchange ?
+          exchange.exchange.content.id
+          :
+          exchange.exchange
   )
 
   if (justFollowing) {
     exchanges = exchanges.filter(exchange =>
-        exchange.exchange.content.exchange
+        exchange.exchange ?
+            exchange.exchange.content.exchange
+            :
+            exchange.exchange
     )
   }
 
   if (exchanges.length > 0) {
     return exchanges.map((exchange, i) =>
-        <Exchange key={i} data={exchange.exchange.content}/>
+        <Exchange key={i} data={exchange.exchange ? exchange.exchange.content : exchange.exchange}/>
     )
   }
   else if (!props.loading) {
