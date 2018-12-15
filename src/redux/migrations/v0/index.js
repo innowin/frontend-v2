81f43c7a19cb19ff510230b286233bb5c27c6d50
mohsen <mohsen.workman@gmail.com>
2018-12-15 14:31:58 +0330

    add translate updater to migration

--- src/redux/migrations/v0/index.js
+++ src/redux/migrations/v0/index.js
@@ -1,8 +1,10 @@
 import auth from './auth'
+import translate from './translate'
 
 export default {
 	ROOT: state => ({
     ...state,
     auth: auth(state.auth),
+    intl: translate(state.intl)
   }),
 }
