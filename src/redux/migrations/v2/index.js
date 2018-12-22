ae18edd8a80175e6885b782f3e7a3e8e72e5c5c6
Mohammad <mohammad.hooshdar@yahoo.com>
2018-12-22 15:29:25 +0330

    password recovery debuged, user side bar social fixed to bottom, create education option field default works now

--- src/redux/migrations/v2/index.js
+++ src/redux/migrations/v2/index.js
@@ -1,8 +1,10 @@
 import temp from './temp'
+import translate from "../v2/translate";
 
 export default {
   ROOT: state => ({
     ...state,
     temp: temp(state.temp),
+    intl: translate(state.intl)
   }),
 }
--- src/views/common/inputs/reduxFormRenderSelectField.js
+++ src/views/common/inputs/reduxFormRenderSelectField.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import constants from "src/consts/constants";
 
-const renderSelectFieldRedux = ({
-                                  input,
-                                  label,
-                                  className,
-                                  placeholder,
-                                  id,
-                                  textFieldClass,
-                                  options,
-                                  meta: {touched, error}
-                                }) => (
-    <div className={className}>
-      <select id={id} className={(touched && error) ? `${textFieldClass} error` : `${textFieldClass}`} {...input}
-              placeholder={placeholder || label}>
-        {options.map(option => <option>{option}</option>)}
-      </select>
-      {(touched && error) && <span className="error-message">{error}</span>}
-    </div>
-)
+const renderSelectFieldRedux = (props) => {
+  const {input, label, className, placeholder, id, textFieldClass, meta, selected, translate} = props
+  const {touched, error} = meta
+  let options = []
+  for (let grade in constants.SERVER_GRADES) {
+    options.push(constants.SERVER_GRADES[grade])
+  }
+  return (
+      <div className={className}>
+        <select id={id} className={(touched && error) ? `${textFieldClass} error` : `${textFieldClass}`} {...input}
+                placeholder={placeholder || label}
+                value={selected}>
+          {options.map(option => <option value={option}>{translate[option]}</option>)}
+        </select>
+        {(touched && error) && <span className="error-message">{error}</span>}
+      </div>
+  )
+}
 export default renderSelectFieldRedux
 
