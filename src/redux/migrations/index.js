eef942ed50eb091f3f153207173f1f72b2c4ec27
Mohammad <mohammad.hooshdar@yahoo.com>
2019-01-07 16:17:26 +0330

    migration version changed to 5

--- src/redux/migrations/index.js
+++ src/redux/migrations/index.js
@@ -3,6 +3,7 @@ import VERSION_1 from './v1'
 import VERSION_2 from './v2'
 import VERSION_3 from './v3'
 import VERSION_4 from './v4'
+import VERSION_5 from './v5'
 
 export default {
   LATEST_VERSION: 4,
@@ -12,5 +13,6 @@ export default {
     2: VERSION_2.ROOT,
     3: VERSION_3.ROOT,
     4: VERSION_4.ROOT,
+    5: VERSION_5.ROOT,
   },
 }
