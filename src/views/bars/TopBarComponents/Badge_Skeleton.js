f7d96cd42719dbbaf9583d129b04d8843840fc0f
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2019-01-09 16:20:52 +0330

    some new fixes

--- src/views/bars/TopBarComponents/Badge_Skeleton.js
+++ src/views/bars/TopBarComponents/Badge_Skeleton.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import DefaultImage from "../../../images/defaults/defaultImage"
+import DefaultImage from '../../../images/defaults/defaultImage'
 
 export default () => {
   return (
       <div className='introduce-badges-item-container'>
-        <DefaultImage className='introduce-badges-media-skeleton'/>
-        <div className='introduce-badges-item-text-container'>
-          <div className='introduce-badges-title-skeleton'>
-            <span> </span>
-          </div>
-          <div className='introduce-badges-description-skeleton'>
-            <span> </span>
+        <div>
+          <DefaultImage className='introduce-badges-media-skeleton'/>
+          <div className='introduce-badges-item-text-container'>
+            <div className='introduce-badges-title-skeleton'>
+              <span> </span>
+            </div>
+            <div className='introduce-badges-description-skeleton'>
+              <span> </span>
+            </div>
           </div>
         </div>
         <div className='bright-line-badge'/>
