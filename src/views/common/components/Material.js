6c00876af3452aaf8080b6251d2869c029b4745c
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2019-01-10 13:17:32 +0330

    material try catch

--- src/views/common/components/Material.js
+++ src/views/common/components/Material.js
@@ -19,75 +19,71 @@ class Material extends React.Component {
   }
 
   handleButtonRelease(e) {
-    try {
-      clearTimeout(this.buttonPressTimer)
-      if (!this.ripple) {
-        let target = this.container
-        let rect = target.getBoundingClientRect()
-        let ripple = document.createElement('span')
-        ripple.className = 'ripple'
-        if (this.props.backgroundColor) ripple.style.backgroundColor = this.props.backgroundColor
-        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
-        target.appendChild(ripple)
-        let top = e.pageY - rect.top - ripple.offsetHeight / 2 - window.scrollY
-        let left = e.pageX - rect.left - ripple.offsetWidth / 2 - window.scrollX
-        ripple.style.top = top + 'px'
-        ripple.style.left = left + 'px'
-        setTimeout(() => {
-          target.removeChild(ripple)
-        }, 600)
-      }
-      else {
-        this.ripple.style.opacity = '0'
-        setTimeout(() => {
-          if (this.ripple && this.container) {
+    clearTimeout(this.buttonPressTimer)
+    if (!this.ripple) {
+      let target = this.container
+      let rect = target.getBoundingClientRect()
+      let ripple = document.createElement('span')
+      ripple.className = 'ripple'
+      if (this.props.backgroundColor) ripple.style.backgroundColor = this.props.backgroundColor
+      ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
+      target.appendChild(ripple)
+      let top = e.pageY - rect.top - ripple.offsetHeight / 2 - window.scrollY
+      let left = e.pageX - rect.left - ripple.offsetWidth / 2 - window.scrollX
+      ripple.style.top = top + 'px'
+      ripple.style.left = left + 'px'
+      setTimeout(() => {
+        target.removeChild(ripple)
+      }, 600)
+    }
+    else {
+      this.ripple.style.opacity = '0'
+      setTimeout(() => {
+        if (this.ripple && this.container) {
+          try {
             this.container.removeChild(this.ripple)
             this.ripple = null
+          } catch (e) {
+            console.log('material failed')
           }
-        }, 500)
-      }
-    } catch (e) {
-      console.log('Material Effect Failed!')
+        }
+      }, 500)
     }
   }
 
   onMouseDown(e) {
-    try {
-      let pageY = e.pageY
-      let pageX = e.pageX
-      this.buttonPressTimer = setTimeout(() => {
-        let target = this.container
-        let rect = target.getBoundingClientRect()
-        let ripple = document.createElement('span')
-        ripple.className = 'rippleSlow'
-        if (this.props.backgroundColor) ripple.style.backgroundColor = this.props.backgroundColor
-        ripple.style.height = ripple.style.width = parseInt(1.3 * Math.max(rect.width, rect.height), 10) + 'px'
-        target.appendChild(ripple)
-        this.ripple = ripple
-        let top = pageY - rect.top - ripple.offsetHeight / 2 - window.scrollY
-        let left = pageX - rect.left - ripple.offsetWidth / 2 - window.scrollX
-        ripple.style.top = top + 'px'
-        ripple.style.left = left + 'px'
-      }, 300)
-    } catch (e) {
-      console.log('Material Effect Failed!')
-    }
+    let pageY = e.pageY
+    let pageX = e.pageX
+    this.buttonPressTimer = setTimeout(() => {
+      let target = this.container
+      let rect = target.getBoundingClientRect()
+      let ripple = document.createElement('span')
+      ripple.className = 'rippleSlow'
+      if (this.props.backgroundColor) ripple.style.backgroundColor = this.props.backgroundColor
+      ripple.style.height = ripple.style.width = parseInt(1.3 * Math.max(rect.width, rect.height), 10) + 'px'
+      target.appendChild(ripple)
+      this.ripple = ripple
+      let top = pageY - rect.top - ripple.offsetHeight / 2 - window.scrollY
+      let left = pageX - rect.left - ripple.offsetWidth / 2 - window.scrollX
+      ripple.style.top = top + 'px'
+      ripple.style.left = left + 'px'
+    }, 300)
   }
 
   handleLeave() {
-    try {
-      clearTimeout(this.buttonPressTimer)
-      if (this.ripple) {
-        this.ripple.style.opacity = '0'
-        setTimeout(() => {
-          if (this.ripple && this.container) {
+    clearTimeout(this.buttonPressTimer)
+    if (this.ripple) {
+      this.ripple.style.opacity = '0'
+      setTimeout(() => {
+        if (this.ripple && this.container) {
+          try {
             this.container.removeChild(this.ripple)
             this.ripple = null
+          } catch (e) {
+            console.log('material failed')
           }
-        }, 500)
-      }
-    } catch (e) {
-      console.log('Material Effect Failed!')
+        }
+      }, 500)
     }
   }
 
@@ -95,7 +91,7 @@ class Material extends React.Component {
     return <div ref={e => this.container = e} className={this.props.className ? 'material ' + this.props.className : 'material'}
                 onMouseDown={this.onMouseDown} onMouseUp={this.handleButtonRelease} onMouseLeave={this.handleLeave} onClick={this.props.onClick}>
       {/*<div className='material-content'>*/}
-        {this.props.content}
+      {this.props.content}
       {/*</div>*/}
     </div>
   }
