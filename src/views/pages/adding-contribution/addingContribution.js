814707de9673d8f6c527a6df179996159d92a45c
Abolfazl Miri <miri1888@gmail.com>
2019-01-23 13:08:14 +0330

    minor fixed

--- src/views/pages/adding-contribution/addingContribution.js
+++ src/views/pages/adding-contribution/addingContribution.js
@@ -991,14 +991,14 @@ class AddingContribution extends PureComponent<AddingContributionProps, AddingCo
   }
 
   shouldComponentUpdate(nextProps, nextState, nextContext): boolean {
-    if (nextProps.categories !== this.props.categories || nextProps.countries !== this.props.countries) {
-      // console.log('USELESS UPDATE STOPPED')
-      return true
-    } else {
-      console.log('AddingContribution-nextProps', nextProps)
-      console.log('AddingContribution-this.props', this.props)
-      return false
-    }
+    return true // Let's see if it's ok?
+    // if (nextProps.categories === this.props.categories || nextProps.countries === this.props.countries || this.state === nextState) {
+    //   console.log('AddingContribution-nextProps', nextProps)
+    //   console.log('AddingContribution-this.props', this.props)
+    //   return false
+    // } else {
+    //   return true
+    // }
   }
 
   render() {
