f2ff55881bc51e74a367ff7ab836467c0f068126
Pedram Rastegar <peddy.rast@gmail.com>
2019-02-18 10:49:37 +0330

    closer

--- src/views/pages/modal/modal.js
+++ src/views/pages/modal/modal.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {CloseIconSvg} from "src/images/icons"
 
 export default ({children,open, closer}) => open ? ReactDOM.createPortal(
 		<React.Fragment>
 			<div className="modal-wrapper">
-				<div className="modal-content-wrapper">{children}</div>
-				<div className="closer" onClick={closer}></div>
+				<div className="modal-content-wrapper">
+					<CloseIconSvg className='close-button pulse' onClick={closer}/>
+					{children}
+				</div>
+				<div className="closer" onClick={closer}/>
 			</div>
 		</React.Fragment>
 		, document.getElementById("modal_container")) : null
