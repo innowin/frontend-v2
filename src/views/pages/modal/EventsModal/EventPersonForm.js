3402b2775e963c03060cb0883dea39ea0d1ec403
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2019-02-18 10:21:05 +0330

    events folder and eventPersonForm file added

---
+++ src/views/pages/modal/EventsModal/EventPersonForm.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import {Component} from 'react'
+
+class EventPersonForm extends Component {
+  render = () => (
+      <React.Fragment>
+        EventPersonForm
+      </React.Fragment>
+  )
+}
+
+export default EventPersonForm
