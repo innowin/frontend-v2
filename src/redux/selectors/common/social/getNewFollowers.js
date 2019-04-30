6f061013a6d9990e3d7c8f0d7d6cb2f07fc338d8
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-04-30 22:32:26 +0430

    fix

--- src/redux/selectors/common/social/getNewFollowers.js
+++ src/redux/selectors/common/social/getNewFollowers.js
@@ -1,23 +1,21 @@
-import {createSelector} from "reselect"
+import {createSelector} from 'reselect'
 
 const getUsers = state => state.identities.list
 const getFollows = state => state.common.social.follows.list
-const clientId = state => state.auth.client.identity.content
-
 const userId = (state, props) => props.userId
 
 export const getFollowersSelector = createSelector(
-    [getUsers, getFollows, userId, clientId],
-    (identities, follows, userId, clientId) => {
+    [getUsers, getFollows, userId],
+    (identities, follows, userId) => {
       if (userId) {
         let user = identities[userId]
         let user_follows_arr = user.social && user.social.follows.content
         let user_follows_obj = {}
         user_follows_arr && user_follows_arr.forEach(id => follows[id] ? user_follows_obj[id] = follows[id] : null)
-        let user_follows = Object.values(user_follows_obj).filter(p => p.follow_followed.id === userId)
+        let user_follows = Object.values(user_follows_obj).filter(p => p.follow_followed.id ? p.follow_followed.id === userId : p.follow_followed === userId)
         let followers = []
         user_follows.forEach(p => followers.push(p.follow_follower))
         return followers
       }
-    }
+    },
 )
--- src/redux/selectors/common/social/getNewFollowings.js
+++ src/redux/selectors/common/social/getNewFollowings.js
@@ -1,23 +1,21 @@
-import {createSelector} from "reselect"
+import {createSelector} from 'reselect'
 
 const getUsers = state => state.identities.list
 const getFollows = state => state.common.social.follows.list
-const clientId = state => state.auth.client.identity.content
-
 const userId = (state, props) => props.userId
 
 export const getFollowingsSelector = createSelector(
-    [getUsers, getFollows, userId, clientId],
-    (identities, follows, userId, clientId) => {
+    [getUsers, getFollows, userId],
+    (identities, follows, userId) => {
       if (userId) {
         let user = identities[userId]
         let user_follows_arr = user.social && user.social.follows.content
         let user_follows_obj = {}
         user_follows_arr && user_follows_arr.forEach(id => follows[id] ? user_follows_obj[id] = follows[id] : null)
-        let user_follows = Object.values(user_follows_obj).filter(p => p.follow_follower.id === userId)
+        let user_follows = Object.values(user_follows_obj).filter(p => p.follow_follower.id ? p.follow_follower.id === userId : p.follow_follower === userId)
         let followings = []
         user_follows.forEach(p => followings.push(p.follow_followed))
         return followings
       }
-    }
+    },
 )
