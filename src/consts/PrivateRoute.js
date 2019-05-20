cac0ff42c6dff40b4a7d244b21537a0f612c4771
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-05-21 03:35:19 +0430

    better checking login :)

--- src/consts/PrivateRoute.js
+++ src/consts/PrivateRoute.js
@@ -2,25 +2,15 @@ import React from 'react'
 import {Route, Redirect} from 'react-router-dom'
 import client from './client'
 
-const renderMergedProps = (component, ...rest) => {
-	const finalProps = Object.assign({}, ...rest);
-	return (
-			React.createElement(component, finalProps)
-	);
-};
+const PrivateRoute = ({component, ...rest}) =>
+    <Route {...rest} render={routeProps =>
+        client.isAuthenticated() ?
+            React.createElement(component, {...routeProps, ...rest})
+            :
+            <Redirect to={{
+              pathname: '/login',
+              state: routeProps.location,
+            }}/>
+    }/>
 
-
-const PrivateRoute = ({component,...rest}) => (
-		<Route {...rest} render={ routeProps => (
-			client.isAuthenticated() ?
-					renderMergedProps(component,routeProps , rest)
-					:
-					<Redirect to={{
-						pathname: '/login',
-						state: routeProps.location,
-					}}/>
-			)}
-		/>
-);
-
-export default PrivateRoute;
+export default PrivateRoute
