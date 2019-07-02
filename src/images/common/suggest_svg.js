8b04812a996eee617905796c41b7a2e08ec8169d
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-07-02 09:17:31 +0430

    suggest icon

---
+++ src/images/common/suggest_svg.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SuggestSvg = (props) =>
+    <svg className={props.className} viewBox="0 0 17.01 16">
+      <g transform="translate(-58.775 -9)">
+        <g data-name="Group 470" transform="translate(58.775 9)">
+          <path data-name="Path 453" d="M75.784,179.671v-4.906c0-1.562-1.119-.941-1.119,0v4.91a1.721,1.721,0,0,1-1.717,1.717H61.611a1.721,1.721,0,0,1-1.717-1.717v-4.915c0-.945-1.119-1.567-1.119,0v4.91a2.842,2.842,0,0,0,2.836,2.836H72.948A2.836,2.836,0,0,0,75.784,179.671Z" transform="translate(-58.775 -166.507)"/>
+          <path data-name="Path 454" d="M149.063,4.775a.557.557,0,0,0,.79,0L152.731,1.9v9.079a.562.562,0,0,0,1.124,0V1.9l2.878,2.878a.56.56,0,0,0,.795-.79L153.694.158a.574.574,0,0,0-.79,0L149.072,3.99A.544.544,0,0,0,149.063,4.775Z" transform="translate(-144.783 0)"/>
+        </g>
+      </g>
+    </svg>
+
+export default SuggestSvg
