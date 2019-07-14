635db93f02791785130e2105dbbd44be6fe2f3a9
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-07-14 18:09:05 +0430

    exvhange fix

--- src/redux/reducers/sliceReducers/exchange/getExchangeByExId.js
+++ src/redux/reducers/sliceReducers/exchange/getExchangeByExId.js
@@ -1,20 +1,12 @@
 const success = (state, action) => {
   const {data} = action.payload
-  if (state.list[data.id])
-    return {
-      ...state,
-      list: {
-        ...state.list,
-        [data.id]: {
-          ...state.list[data.id],
-        },
-      },
-    }
-  else return {
+  const previous = state.list[data.id] ? {...state.list[data.id]} : {}
+  return {
     ...state,
     list: {
       ...state.list,
       [data.id]: {
+        ...previous,
         ...data,
       },
     },
