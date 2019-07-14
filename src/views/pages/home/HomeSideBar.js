113215d4c4b5fdc2d19eee802815af236f15c08d
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-07-14 19:43:20 +0430

    ex logo load fix

--- src/views/pages/home/HomeSideBar.js
+++ src/views/pages/home/HomeSideBar.js
@@ -1,102 +1,16 @@
-// @flow
-import * as React from 'react'
+import React from 'react'
 import {PureComponent} from 'react'
 import * as PropTypes from 'prop-types'
 import ExchangeMembershipActions from 'src/redux/actions/commonActions/exchangeMembershipActions'
-import type {exchangeType} from 'src/consts/flowTypes/exchange/exchange.js'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {ChannelIcon, MainLbarArrow} from 'src/images/icons'
 import {getExchangeMembershipsSelector} from 'src/redux/selectors/common/social/getExchangeMemberships'
-import Material from '../../common/components/Material'
 import UserDetailPanel from '../../common/components/UserDetailPanel'
 import {ClipLoader} from 'react-spinners'
+import SideBarItem from './HomeSideBarItem'
 
-type PropsSideBarItem = {
-  exchange: exchangeType,
-  handleClick: Function,
-  active: boolean
-}
-
-export class SideBarItem extends PureComponent<PropsSideBarItem> {
-
-  static propTypes = {
-    exchange: PropTypes.object.isRequired,
-    handleClick: PropTypes.func.isRequired,
-    active: PropTypes.bool,
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      imageLoaded: false,
-    }
-  }
-
-  componentDidMount() {
-    if (this.props.exchange.exchange_image) {
-      let image = new Image()
-      image.src = this.props.exchange.exchange_image.file
-      image.onload = () => {
-        this.setState({...this.state, imageLoaded: true})
-      }
-    }
-  }
-
-  _onClickHandler = () => {
-    const {handleClick, exchange} = this.props
-    handleClick(exchange.id)
-  }
-
-  render() {
-    const {active} = this.props
-    const {exchange_image, name, id: exchangeId} = this.props.exchange
-    return (
-        <div className={`item-wrapper ${active ? 'active' : ''}`} onClick={this._onClickHandler}>
-          <Material content={
-            <div className="header-exchange">
-              <a className="default-logo">
-                {exchange_image && this.state.imageLoaded ?
-                    <img className="img-logo" src={exchange_image.file} alt="logo"/>
-                    :
-                    <ChannelIcon className='default-channel-icon'/>
-                }
-              </a>
-              <div className={`exchange-name ${active && 'active'}`}>{name}
-                <Link to={active ? '/exchange/' + exchangeId : '/'}>
-                  <div className={`exchange-sub-name-link ${active && 'active'}`}>پروفایل پنجره</div>
-                </Link>
-              </div>
-              <div className={active ? 'left-arrow-home-exchange-container' : 'left-arrow-home-exchange-container-hide'}>
-                <MainLbarArrow className="home-exchange-left-arrow"/>
-              </div>
-            </div>
-          }/>
-        </div>
-    )
-  }
-}
-
-
-type StateHomeSideBar = {|
-  activeId: ?number,
-|}
-
-type PropsHomeSideBar = {|
-  identityId: number,
-  activeExchangeId: ?number,
-  setExchangeId: Function,
-  classNames?: string,
-  clientExchanges: ({ id: number })[],
-  isLoading: boolean,
-  error: ?string,
-  actions: {
-    getExchangeMembershipByMemberIdentity: Function
-  },
-|}
-
-class HomeSideBar extends PureComponent<PropsHomeSideBar, StateHomeSideBar> {
+class HomeSideBar extends PureComponent {
   static propTypes = {
     identityId: PropTypes.number.isRequired,
     activeExchangeId: PropTypes.number,
---
+++ src/views/pages/home/HomeSideBarItem.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Material from '../../common/components/Material'
+import {ChannelIcon, MainLbarArrow} from '../../../images/icons'
+import {Link} from 'react-router-dom'
+
+const SideBarItem = (props) => {
+
+  function _onClickHandler() {
+    const {handleClick, exchange} = props
+    handleClick(exchange.id)
+  }
+
+  const {active} = props
+  const {exchange_image, name, id: exchangeId} = props.exchange
+  return (
+      <div className={`item-wrapper ${active ? 'active' : ''}`} onClick={_onClickHandler}>
+        <Material content={
+          <div className="header-exchange">
+            <a className="default-logo">
+              {
+                exchange_image ?
+                    <img className="img-logo" src={exchange_image.file} alt="logo"/>
+                    :
+                    <ChannelIcon className='default-channel-icon'/>
+              }
+            </a>
+            <div className={`exchange-name ${active && 'active'}`}>{name}
+              <Link to={active ? '/exchange/' + exchangeId : '/'}>
+                <div className={`exchange-sub-name-link ${active && 'active'}`}>پروفایل پنجره</div>
+              </Link>
+            </div>
+            <div className={active ? 'left-arrow-home-exchange-container' : 'left-arrow-home-exchange-container-hide'}>
+              <MainLbarArrow className="home-exchange-left-arrow"/>
+            </div>
+          </div>
+        }/>
+      </div>
+  )
+}
+
+export default SideBarItem
