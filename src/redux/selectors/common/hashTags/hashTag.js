58eaabbb541ba2d92becb8323f2bfb8dc7bd9c2e
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2019-07-23 17:56:41 +0430

    hashtags added to bee

--- src/redux/selectors/common/hashTags/hashTag.js
+++ src/redux/selectors/common/hashTags/hashTag.js
@@ -1,11 +1,11 @@
 import {createSelector} from 'reselect'
 
 const getHashTags = (state) => {
-  return state.common.hashTag.hashTags || {}
+  return state.common.hashTag.hashTags.list || {}
 }
 
 // a small selector for work with hashTags.
 export const hashTagsListSelector = createSelector(
     getHashTags,
-    hashTags => hashTags.list,
+    hashTags => hashTags,
 )
